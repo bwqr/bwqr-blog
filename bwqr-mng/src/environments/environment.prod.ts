c528aeb168977e2e654b06b1550ac4e6615316bf
blowedminds <ruzgardeniz.08@hotmail.com>
2019-07-26 14:55:27 +0300

    styles module updates

--- bwqr-mng/src/environments/environment.prod.ts
+++ bwqr-mng/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://bwqr.test/'
+  apiUrl: 'http://bwqr.com/'
 };
--- bwqr-mng/src/environments/environment.ts
+++ bwqr-mng/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://fm.test'
+  apiUrl: 'http://bwqr.test/'
 };
 
 /*
